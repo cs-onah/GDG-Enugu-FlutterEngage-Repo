9d8d46a172dd606ce7170570ea5dc57d8ff54c2d
csonah <revelationjay02@gmail.com>
2021-03-24 19:28:20 +0100

    built login screen

---
+++ android/app/src/main/kotlin/app/csonah/my_first_flutter_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.csonah.my_first_flutter_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TechoCraft());
+}
+
+
+class TechoCraft extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  //Controllers
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _obscureText = true;
+  bool _checkBoxValue = false;
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text("Login Screen"),
+        centerTitle: false,
+      ),
+      body: Center(
+        child: Form(
+          key: _formKey,
+          child: Container(
+            color: Colors.transparent,
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: SingleChildScrollView(
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 35,),
+                Icon(Icons.agriculture, color: Colors.blue, size: 65,),
+                SizedBox(height: 30,),
+                Text("Agrico", style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold, color: Colors.blue),),
+                SizedBox(height: 30,),
+                //Username
+                TextFormField(
+                  controller: _usernameController,
+                  validator: (val) => val.length < 4 ? "Username too short" : null,
+                  decoration: InputDecoration(
+                    fillColor: Colors.grey[300],
+                    filled: true,
+                    border: InputBorder.none,
+                    hintText: "Username",
+                  ),
+                ),
+                SizedBox(height: 15,),
+                //Password
+                TextFormField(
+                  controller: _passwordController,
+                  validator: (val) => val.length < 4 ? "Password too short" : null,
+                  obscureText: _obscureText,
+                  decoration: InputDecoration(
+                    fillColor: Colors.grey[300],
+                    filled: true,
+                    border: InputBorder.none,
+                    hintText: "Password",
+                    suffixIcon: IconButton(
+                      icon: _obscureText ? Icon(Icons.visibility) : Icon(Icons.visibility_off),
+                      onPressed: (){
+
+                        setState(() {
+                          _obscureText = !_obscureText;
+                        });
+
+                      },
+                    )
+                  ),
+                ),
+                SizedBox(height: 15),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Checkbox(
+                        value: _checkBoxValue,
+                        onChanged: (val){setState(() {
+                      _checkBoxValue = val;
+                    });}),
+                    SizedBox(width: 5,),
+                    Text("I am Human"),
+                  ],
+                ),
+                SizedBox(height: 30),
+                Row(
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                        height: 50,
+                        child: TextButton(
+                          onPressed: (){
+
+                            if (!_formKey.currentState.validate()) return;
+
+                            if(_checkBoxValue == false){
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(content: Text("Please verify your are a human."),));
+                              return;
+                            }
+                            print(
+                              '----------'
+                            );
+                            print(" username: ${_usernameController.text}, password: ${_passwordController.text}, Remember me: $_checkBoxValue}");
+                          },
+                          child: Text("Login",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                        ),
+                      ),),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 20,),
+                Text("Forgot Password?",
+                  style: TextStyle(
+                    color: Colors.grey[600],
+                    fontSize: 16,
+                  ),
+                )
+
+              ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
